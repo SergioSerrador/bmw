719c7102517e8c9e198194a71918a82aa426889e
SergioSerrador <sergio.serrador@colegiomiralmonte.es>
2025-10-29 23:24:22 +0100

    Task 8

--- bmw/users/urls.py
+++ bmw/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('login/', views.logout_user, name='logout'),
 ]
--- bmw/users/views.py
+++ bmw/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -10,3 +11,19 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
